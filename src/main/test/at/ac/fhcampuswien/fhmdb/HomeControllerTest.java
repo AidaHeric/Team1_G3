972f329e2367db6888819e1e4f3046b821e5c897
Anna <anna.zierlinger@stud.fh-campuswien.ac.at>
2024-03-15 13:04:11 +0100

    Tests run when running each on its own

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -1,37 +1,38 @@
 package at.ac.fhcampuswien.fhmdb;
 
 import at.ac.fhcampuswien.fhmdb.models.Movie;
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
+import static javafx.application.Platform.runLater;
 import static org.junit.Assert.*;
 
 
-
 public class HomeControllerTest {
-    //private HomeController controller;
     HomeController controller = new HomeController();
 
     public List<Movie> allMovies = Movie.initializeMovies();
-    List<Movie> filteredMovies = controller.observableMovies;
+    List<Movie> filteredMovies = new ArrayList<>();
 
     @Before
     public void setUp() {
-        controller = new HomeController();
+        //controller = new HomeController();
         Platform.startup(() -> {
             controller.searchField = new TextField();
+            controller.genreComboBox = new JFXComboBox();
+            controller.sortBtn = new JFXButton();
         });
     }
 
@@ -43,22 +44,85 @@ public class HomeControllerTest {
     }*/
 
     @Test
-    public void testGenreFilter() {
-        controller.genreComboBox.setValue("ACTION");
-        controller.handleGenreFilter();
-        filteredMovies = controller.observableMovies;
-        for (Movie movie : filteredMovies) {
-            assertTrue(movie.getGenres().contains("ACTION"));
-        }
+    public void testAllGenresNoQueryAfterFilter(){
+        Platform.runLater(() -> {
+            controller.genreComboBox.setValue("ACTION");
+            controller.handleGenreFilter();
+            controller.searchField.setText("Inception");
+            controller.handleSearch();
+
+            controller.searchField.setText("");
+            controller.genreComboBox.setValue("ALL");
+            controller.handleGenreFilter();
+            controller.handleSearch();
+        });
+
+        Platform.runLater(() -> {
+            assertEquals(allMovies,controller.observableMovies);
+        });
     }
 
-    /*@Test
-    public void testSort() {
-        controller.handleSort();
-        String currentText = controller.sortBtn.getText();
-        assertEquals("Sort (desc)", currentText);
-        controller.handleSort();
-        assertEquals("Sort (asc)", currentText);
-    }*/
+    @Test
+    public void testGenreFilter() {
+        Platform.runLater(() -> {
+            controller.genreComboBox.setValue("ACTION");
+
+            controller.handleGenreFilter();
+        });
+        for (Movie movie : allMovies) {
+            if (movie.getGenres().contains("ACTION")) {
+                filteredMovies.add(movie);
+            }
+        }
+        Platform.runLater(() -> assertTrue("Die Listen enthalten nicht dieselben Elemente.",
+                filteredMovies.size() == controller.observableMovies.size() &&
+                        filteredMovies.containsAll(controller.observableMovies) &&
+                        controller.observableMovies.containsAll(filteredMovies)));
+
+    }
+
+    @Test
+    public void testSortAscendingAndGenreFilter() {
+        Platform.runLater(() -> {
+            controller.genreComboBox.setValue("ACTION");
+            controller.sortBtn.setText("Sort (asc)");
+
+            controller.handleGenreFilter();
+            controller.handleSort();
+        });
+
+        for (Movie movie : allMovies) {
+            if (movie.getGenres().contains("ACTION")) {
+                filteredMovies.add(movie);
+            }
+        }
+        filteredMovies.sort(Comparator.comparing(Movie::getTitle));
+
+        Platform.runLater(() -> {
+            Assertions.assertIterableEquals(filteredMovies, controller.observableMovies);
+        });
+    }
+
+    @Test
+    public void testSortAscending() {
+        Platform.runLater(() -> {
+            controller.sortBtn.setText("Sort (asc)");
+            controller.handleSort();
+                });
+        Platform.runLater(() -> {
+                    assertEquals("Sort (desc)", controller.sortBtn.getText());
+                });
+    }
+
+    @Test
+    public void testSortDescending() {
+        Platform.runLater(() -> {
+            controller.sortBtn.setText("Sort (desc)");
+            controller.handleSort();
+        });
+        Platform.runLater(() -> {
+            assertEquals("Sort (asc)", controller.sortBtn.getText());
+        });
+    }
 
 }

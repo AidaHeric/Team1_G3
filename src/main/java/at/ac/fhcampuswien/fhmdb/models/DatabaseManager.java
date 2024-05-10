2cadafdfa8dcbb3134ce9119b72aa6d2315bba31
Stefanie Biber <stefaniebiber@ied.edu>
2024-05-10 16:23:29 +0200

    UE3: New classes + DAO

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/DatabaseManager.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampuswien.fhmdb.models;
+
+public class DatabaseManager {
+    /*DAO Reference from the OMR Page:
+
+    Dao<Account, String> accountDao =
+            DaoManager.createDao(connectionSource, Account.class);
+    Dao<Order, Integer> orderDao =
+            DaoManager.createDao(connectionSource, Order.class);
+
+     */
+}

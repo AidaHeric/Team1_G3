2b7f7f82bc5d4f59ff822da28ce0a3efd12e53bf
Stefanie Biber <stefaniebiber@ied.edu>
2024-05-10 16:25:27 +0200

    UE3: New classes + DAO

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module at.ac.fhcampuswien.fhmdb {
     requires com.jfoenix;
     requires com.google.gson;
     requires okhttp3;
+    requires ormlite.core;
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
     exports at.ac.fhcampuswien.fhmdb;

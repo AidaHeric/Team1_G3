92cc49dced34275044fb8722a9cb2f197c2c4585
Anna <anna.zierlinger@stud.fh-campuswien.ac.at>
2024-05-13 18:26:30 +0200

    movies from api to database

--- src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/FhmdbApplication.java
@@ -1,5 +1,9 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import at.ac.fhcampuswien.fhmdb.database.DatabaseManager;
+import at.ac.fhcampuswien.fhmdb.database.MovieRepository;
+import at.ac.fhcampuswien.fhmdb.exceptions.DatabaseException;
+import at.ac.fhcampuswien.fhmdb.models.MovieAPI;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -11,19 +15,24 @@ import java.util.Objects;
 // Main application
 public class FhmdbApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        //Load the FXML file
+    public void start(Stage stage) throws IOException, DatabaseException {
         FXMLLoader fxmlLoader = new FXMLLoader(FhmdbApplication.class.getResource("home-view.fxml"));
-        // Create a scene witht he loaded FXML content
         Scene scene = new Scene(fxmlLoader.load(), 890, 620);
-        // Add CSS styles to the scene
         scene.getStylesheets().add(Objects.requireNonNull(FhmdbApplication.class.getResource("styles.css")).toExternalForm());
-        // Set title of the stage
         stage.setTitle("FHMDb");
-        // Set scene to the stage
         stage.setScene(scene);
-        // Display the stage
         stage.show();
+
+        MovieRepository movieRepository = new MovieRepository();
+        MovieAPI movieAPI = new MovieAPI();
+        DatabaseManager.getDatabase();
+        if (movieAPI.getAllMovies() != null) {
+            movieRepository.removeAll();
+            movieRepository.addAllMovies(movieAPI.getAllMovies());
+        }
+
+
+        //DatabaseManager.getDatabase().testDB();
     }
 
     public static void main(String[] args) {
